7810ae925b599396f86a30279c5361e78c0572d7
Gabriela Paulino <gabrielappsilva18@gmail.com>
2024-01-23 20:11:38 -0300

    aula 8

---
+++ aulas/aula 08/exercicioHeranca.js
@@ -0,0 +1,61 @@
+class Animal {
+    //3 básicas
+    //nasce, crece, reproduz, morre
+}
+
+class Mamifero {
+    //1 específica
+    //amamentar
+}
+
+class Reptil {
+    //1 específica
+}
+
+class Animal {
+    //3 propriedades básicas (peso, altura, velocidade)
+    constructor(peso, altura, velocidae) {
+        this.peso = peso;
+        this.altura = altura;
+        this.velocidae = velocidae;
+    }
+    //4 comportamentos (nasce, crece, reproduz, morre)
+    nascer() {
+
+    }
+
+    crescer() {
+
+    }
+
+    reproduzir() {
+
+    }
+
+    morrer() {
+
+    }
+}
+
+class Mamifero extends Animal {
+    //1 específica
+    constructor(peso, altura, velocidae, mamas){
+        super(peso, altura, velocidae)
+        this.mamas = mamas;
+    }
+    
+    amamentar() {
+
+    }
+}
+
+class Reptil extends Animal {
+    constructor(peso, altura, velocidae, escamas) {
+        super(peso, altura, velocidae);
+        this.escamas = escamas;
+    }
+    
+    pesilhotermia() {
+
+    }
+}
---
+++ aulas/aula 08/script.js
@@ -0,0 +1,20 @@
+// let produtos = [];
+// let cadastrarNovoProduto = true;
+
+// while(cadastrarNovoProduto) {
+//     let produto = {}
+
+//     //solicitar valores
+//     produto.nome = prompt("Informe o nome do produto:");
+//     produto.peso = prompt("Informe o peso do produto:");
+//     produto.preco = prompt("Informe o preco do produto:");
+//     produto.vencimento = prompt("Informe o vencimento do produto:");
+    
+//     produtos.push(produto);
+
+//     // Perguntar ao usuário se deseja cadastrar outro produto
+//     let resposta = prompt("Deseja cadastrar outro produto? (Digite 'sim' para continuar):");
+//     cadastrarNovoProduto = (resposta.toLowerCase() === 'sim');
+// }
+
+// console.log("Lista de produtos: ", produtos)
