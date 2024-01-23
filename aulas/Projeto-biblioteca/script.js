cd00d987f5974691c52cacb763ea643886cf2684
Gabriela Paulino <gabrielappsilva18@gmail.com>
2024-01-23 20:34:50 -0300

    aula 8

--- aulas/Projeto-biblioteca/script.js
+++ aulas/Projeto-biblioteca/script.js
@@ -5,6 +5,29 @@ class MaterialBibliografico {
         this.disponivel = true;
     }
 
+    realizarEmprestimo() {
+        if (this.disponivel == true) {
+            this.disponivel = false;
+            return true; //Emprestimo realizado com sucesso
+        } else if (this.disponivel == false) {
+            return false; //Material ja emprestado
+        }
+    }
+
+    realizarDevolucao() {
+        if (this.disponivel == false) {
+            this.disponivel = true;
+            return true; //Devolução realizada com sucesso
+        }else {
+            return false; //Material já devolvido previamente
+        }
+    }
+
+    realizarDevolucao2 (){
+        if(this.disponivel){
+            return false;
+        }
+    }
 }
 
 class Livro extends MaterialBibliografico {
