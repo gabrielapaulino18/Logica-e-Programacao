46213ccd09a56ad481804f2ec645a53c621ea50b
Gabriela Paulino <gabrielappsilva18@gmail.com>
2024-01-29 21:21:05 -0300

    Aula 10 Jogo e Personagens

---
+++ aulas/aula 10/script.js
@@ -0,0 +1,76 @@
+/* 
+                            Parte 01: 
+
+Crie uma classe Personagem. O personagem deverá ter:
+nome, vida, ataque, defesa, vivo (ou morto), posicao (numero inteiro)
+lembre de criar o método construtor 
+
+                            Parte 02: 
+
+Crie a função/método morrer
+    - propriedade "vivo" se torna false, e console.log("morreu")
+
+Crie a função tomarDano
+    - tomar dano recebe um parametro, que é o valor do dano
+    - diminui os pontos de vida do personagem, com base no dano tomado
+    - verifica, se o dano for maior que os pontos de vida, personagem morre
+    - se os pontos de vida forem menor ou igual a zero, personagem morre
+    - ao morrer, personagem continua com 0 pontos de vida
+    - o personagem só pode tomar dano se estiver vivo
+
+
+*/
+
+
+
+class Personagem {
+    constructor(nome, ataque, defesa, vida, posicao, vivo = true) {
+        this.nome = nome;
+        this.ataque = ataque;
+        this.defesa = defesa;
+        this.posicao = posicao;
+        this.vida = vida;
+        this.vivo = vivo;
+    }
+
+    morrer() {
+        this.vivo = false;
+        console.log(`O ${this.nome} morreu!`);
+    }
+
+    tomarDano(quantidade) {
+        if (this.vivo) {            
+            this.vida = this.vida - quantidade;
+            console.log(`${this.nome} sofreu ${quantidade} de dano, e agora tem ${this.vida} de vida`)
+
+            if (this.vida <= 0) {
+                this.vida = 0;
+                this.morrer();
+            }
+        } else {
+            console.log(`${this.nome} não pode mais sofrer dano!`);
+        }
+    }
+
+    atacar(inimigo) {
+        if(inimigo.vivo && this.vivo) {
+            console.log(`${this.nome} atacou ${inimigo.nome} com força de ${this.ataque}`);
+            inimigo.tomarDano(this.ataque);
+        } else {
+            console.log("Não é possível atacar na condição de morto!");
+        }
+    }
+}
+
+let personagem1 = new Personagem("Arthur", 10, 12, 100, 1, true);
+let personagem2 = new Personagem("Gendalf", 12, 8, 85, 1);
+
+console.log(personagem1);
+console.log(personagem2);
+
+console.log(personagem1.atacar(personagem2));
+console.log(personagem2.atacar(personagem1));
+
+while(personagem1.vivo) {
+    personagem2.atacar(personagem1);
+}
