020d8b23e578daf96cf2cc580289dae08b28fa39
Gabriela Paulino <gabrielappsilva18@gmail.com>
2024-02-01 20:19:05 -0300

    Projeto de Cadastro de Veiculo

---
+++ aulas/script.js
@@ -0,0 +1,28 @@
+/*
+#Parte 1:
+    - Crie uma classe Veiculo
+    - Adicione as propriedades: marca, modelo, preco, cor, autonomia, capacidadeTanque, imagemURL
+    - Adicione um metodo calcularDistanciaMaxia, que retornará a autonomia * capacidadeTanque
+    - Adicione um metodo exibirDetalhes, que retornará os dados concatenados de:
+        * marca, modelo, cor, preco.toFixed(2)
+*/
+
+class Veiculo {
+    constructor(marca, modelo, preco, cor, autonomia, capacidadeTanque, imagemURL) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.preco = preco;
+        this.cor = cor;
+        this.autonomia = autonomia;
+        this.capacidadeTanque = capacidadeTanque;
+        this.imagemURL = imagemURL;
+    }
+
+    calcularDistanciaMaxia () {
+
+    }
+
+    exibirDetalhes () {
+        
+    }
+}
