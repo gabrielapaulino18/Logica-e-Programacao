80e4f4cd94fd88936a0310b250c0096e110b9ed5
Gabriela Paulino <gabrielappsilva18@gmail.com>
2024-02-01 21:41:41 -0300

    cadastro de veiculos

---
+++ aulas/Projeto-Cadastro-Veiculo/script.js
@@ -0,0 +1,61 @@
+/*
+#Parte 1:
+    - Crie uma classe Veiculo
+    - Adicione as propriedades: marca, modelo, preco, cor, autonomia, capacidadeTanque, imagemURL
+    - Adicione um metodo calcularDistanciaMaxia, que retornará a autonomia * capacidadeTanque
+    - Adicione um metodo exibirDetalhes, que retornará os dados concatenados de:
+        * marca, modelo, cor, preco.toFixed(2)
+*/
+
+class Veiculo {
+    constructor(marca, modelo, preco, cor, autonomia, capacidadeTanque, imagemURL) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.preco = preco;
+        this.cor = cor;
+        this.autonomia = autonomia;
+        this.capacidadeTanque = capacidadeTanque;
+        this.imagemURL = imagemURL;
+    }
+
+    calcularDistanciaMaxima() {
+        return this.autonomia * this.capacidadeTanque;
+    }
+
+    exibirDetalhes() {
+        return `${this.marca} ${this.modelo} - ${this.cor} - R$ ${this.preco.toFixed(2)}`;
+    }
+}
+
+let veiculos = [];
+
+// Função para cadastrar veículo
+function cadastrarVeiculo() {
+    //recebimento de valores do HTML
+    const marca = document.getElementById("marca").value;
+    const modelo = document.getElementById("modelo").value;
+    const preco = parseFloat(document.getElementById("preco").value);
+    const cor = document.getElementById("cor").value;
+    const autonomia = parseInt(document.getElementById("autonomia").value);
+    const capacidadeTanque = parseInt(document.getElementById("capacidadeTanque").value);
+    const imagemURL = document.getElementById("imagemURL").value;
+
+    // Instanciar um novo objeto veículo, passando os valores pedidos no construtor
+    const veiculo = new Veiculo(marca, modelo, preco, cor, autonomia, capacidadeTanque, imagemURL);
+
+    // Adicionar o veículo a nossa lista "banco de dados"
+    veiculos.push(veiculo);
+
+    // Atualiza a exibição
+    console.log(veiculos);
+
+    // Limpar formulário
+    document.getElementById("veiculoForm").reset();
+}
+
+
+// veiculos.push(new Veiculo("fiat", "uno", 60000, "branco", 16, 90, "www.url.com"));
+// veiculos.push(new Veiculo("fiat", "palio", 90000, "vermelho", 14, 90, "www.url88.com"));
+// veiculos.push(new Veiculo("chrevrolet", "cruze", 120000, "amarelo", 11, 90, "www.url45.com"));
+
+// console.log(veiculos);
